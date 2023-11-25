dc7e9c5e99a55e5eb255efc878c6760cf15ec7cd
freitas <joaoflaviofreitas@gmail.com>
2023-11-25 14:48:39 -0300

    Add project files.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,72 @@
+﻿
+
+using System;
+
+namespace Game
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+
+            Console.WriteLine("Welcome to the Rock Paper Scissors Game!!");
+            Console.WriteLine("Enter 'r' for Rock, 'p' for Paper, 's' to Scissors");
+            string input = Console.ReadLine();
+
+            int enemyChoice = random.Next(0, 3);
+
+            if(enemyChoice == 0 )
+            {
+
+                Console.WriteLine("enemy chooses rock");
+                switch (input)
+                {
+                    case "r":
+                        Console.WriteLine("Tie");
+                        break;
+                    case "s":
+                        Console.WriteLine("You lose");
+                        break;
+                    case "p":
+                        Console.WriteLine("You win");
+                        break;
+                }
+            } else if(enemyChoice == 1 )
+            {
+                Console.WriteLine("enemy chooses paper");
+                switch (input)
+                {
+                    case "r":
+                        Console.WriteLine("You lose");
+                        break;
+                    case "s":
+                        Console.WriteLine("You win");
+                        break;
+                    case "p":
+                        Console.WriteLine("Tie");
+                        break;
+                }
+            } else { Console.WriteLine("enemy chooses scissors");
+                switch (input)
+                {
+                    case "r":
+                        Console.WriteLine("You win");
+                        break;
+                    case "s":
+                        Console.WriteLine("Tie");
+                        break;
+                    case "p":
+                        Console.WriteLine("You lose");
+                        break;
+                }
+            }
+
+            
+        }
+    }
+}
